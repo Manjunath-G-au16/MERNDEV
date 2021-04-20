94dc0af9c81bdf290052f6aebcfe0421a339b482
modimanju <modimanju2019@gmail.com>
2021-04-20 11:59:43 +0530

    connected mogodb

--- Server/app.js
+++ Server/app.js
@@ -1,5 +1,20 @@
+const mongoose = require("mongoose");
 const express = require("express");
 const app = express();
+const DB =
+  "mongodb+srv://modimanju:123088@cluster0.3mt9q.mongodb.net/merndev?retryWrites=true&w=majority";
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+  })
+  .then(() => {
+    console.log("connection successful");
+  })
+  .catch((err) => console.log("connection unsuccessful"));
 
 const middleware = (req, res, next) => {
   console.log("Hello from middleware");
