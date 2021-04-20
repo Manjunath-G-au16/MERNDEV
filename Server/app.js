3dce3eb61fa6f30c3baf979322b48911c4cf576c
modimanju <modimanju2019@gmail.com>
2021-04-20 15:23:24 +0530

    router added

--- Server/app.js
+++ Server/app.js
@@ -7,6 +7,10 @@ dotenv.config({ path: "./config.env" });
 
 require("./db/conn");
 
+app.use(express.json());
+
+app.use(require("./router/auth"));
+
 const PORT = process.env.PORT;
 
 const middleware = (req, res, next) => {
