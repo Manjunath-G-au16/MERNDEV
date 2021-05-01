a6f3e43e0521fd09da0485f009f827e67922fa7e
modimanju <modimanju2019@gmail.com>
2021-05-01 12:41:39 +0530

    Route Setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,44 @@
-import React from 'react'
+import React from "react";
+import Navbar from "./components/Navbar";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Redirect,
+} from "react-router-dom";
+import Home from "./components/Home";
+import About from "./components/About";
+import Signin from "./components/Signin";
+import Signup from "./components/Signup";
+import Contact from "./components/Contact";
 
 function App() {
   return (
-    <div>
-      <h1>MernDev</h1>
-    </div>
-  )
+    <>
+      <Router>
+        <main>
+          <Switch>
+            <Route path="/" exact>
+              <Home />
+            </Route>
+            <Route path="/about" exact>
+              <About />
+            </Route>
+            <Route path="/signin" exact>
+              <Signin />
+            </Route>
+            <Route path="/signup" exact>
+              <Signup />
+            </Route>
+            <Route path="/contact" exact>
+              <Contact />
+            </Route>
+            <Redirect to="/" />
+          </Switch>
+        </main>
+      </Router>
+    </>
+  );
 }
 
-export default App
+export default App;
