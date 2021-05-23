b66b93d11b5036d107df15a26386a659b5d9d5d7
modimanju <modimanju2019@gmail.com>
2021-05-23 21:17:55 +0530

    Update profile functionality

--- client/src/components/Update.js
+++ client/src/components/Update.js
@@ -1,6 +1,9 @@
 import React, { useEffect, useState } from "react";
 
 const Update = () => {
+  const [msg, setMsg] = useState([{
+    messages:""
+  }]);
   const [userData, setUserData] = useState({
     name: "",
     email: "",
@@ -18,8 +21,10 @@ const Update = () => {
       });
       const data = await res.json();
       console.log(data);
+      setMsg(data.messages);
       setUserData({
         ...userData,
+        pic: data.pic,
         name: data.name,
         email: data.email,
         phone: data.phone,
@@ -44,11 +49,51 @@ const Update = () => {
     const value = e.target.value;
     setUserData({ ...userData, [name]: value });
   };
+  const handleMsg = (e) => {
+    const name = e.target.name;
+    const value = e.target.value;
+    setUserData({ ...msg, [name]: value });
+  };
+  //Pic Update
+  const [image, setImage] = useState("");
+  const [picc, setPic] = useState("");
+  const updatePic = () => {
+    const data = new FormData();
+    data.append("file", image);
+    data.append("upload_preset", "merndev");
+    data.append("cloud_name", "modimanju");
+    fetch("https://api.cloudinary.com/v1_1/modimanju/image/upload", {
+      method: "post",
+      body: data,
+    })
+      .then((res) => res.json())
+      .then((data) => {
+        console.log(data.secure_url);
+        setPic(data.secure_url);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+
+    console.log("pic:", picc);
+    // const id = prompt("enter id");
+    // const newPic = prompt("Enter pic url");
+    // fetch("/updatePic", {
+    //   method: "put",
+    //   headers: {
+    //     "Content-Type": "application/json",
+    //   },
+    //   body: JSON.stringify({ newPic: newPic, id: id }),
+    // });
+  };
 
   //sending data to backend
   const updateForm = (e) => {
     e.preventDefault();
     const { name, email, phone, work, id } = userData;
+    const pic = picc;
+    const messages = msg;
+    // const pic = data.url;
     console.log(userData);
 
     fetch("/edit", {
@@ -57,15 +102,15 @@ const Update = () => {
         "Content-Type": "application/json",
       },
       body: JSON.stringify({
+        pic,
         name,
         email,
         phone,
         work,
         id,
-      })
-    }
-    );
-
+        messages,
+      }),
+    });
   };
 
   return (
@@ -76,6 +121,15 @@ const Update = () => {
       <br />
       <h1>Update</h1>
       <form method="POST">
+        {/* <input
+          type="text"
+          className="input"
+          name="pic"
+          value={picc}
+          onUpdate={handleInputs}
+          placeholder="pic"
+        /> */}
+
         <input
           type="text"
           className="input"
@@ -109,6 +163,7 @@ const Update = () => {
           onChange={handleInputs}
           placeholder="work"
         />
+ 
         <input
           type="submit"
           className="button"
@@ -118,6 +173,31 @@ const Update = () => {
           onClick={updateForm}
         />
       </form>
+      {msg.map((item) => {
+          return (
+            <input
+              type="text"
+              className="input"
+              name="message"
+              value={item.message}
+              onChange={handleMsg}
+              placeholder="message"
+            />
+          );
+        })}
+      <input
+        type="file"
+        className="input"
+        name="file"
+        onChange={(e) => setImage(e.target.files[0])}
+      />
+      <button
+        onClick={() => {
+          updatePic(userData._id);
+        }}
+      >
+        upload
+      </button>
     </div>
   );
 };
