1e8dbd75587faf89cb7e8fcaa428695566054ea4
modimanju <modimanju2019@gmail.com>
2021-05-23 21:20:12 +0530

    Find, Display, Update user routes added

--- Server/router/auth.js
+++ Server/router/auth.js
@@ -5,6 +5,8 @@ const User = require("../model/userSchema");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
 const authenticate = require("../middleware/authenticate");
+const finduser = require("../middleware/finduser");
+const displayuser = require("../middleware/displayuser");
 
 router.get("/", (req, res) => {
   res.send("HomePage Server from router");
@@ -102,6 +104,37 @@ router.get("/about", authenticate, (req, res) => {
   console.log("Hello from About");
   res.send(req.rootUser);
 });
+//User Data
+router.get("/userdata", authenticate, (req, res) => {
+  console.log("Hello from Userdata");
+  res.send(req.rootUser);
+});
+//Find
+router.post("/finduser", finduser, (req, res) => {
+  console.log("Hello from Userdata");
+  res.send(req.rootUser);
+});
+//Display all
+router.post("/displayuser", displayuser, (req, res) => {
+  console.log("Hello from Userdata");
+  res.send(req.rootUser);
+});
+//Finduser
+// router.get("/finduser", async (req, res) => {
+//   try {
+//     const username = "test3";
+//     const rootUser = await User.findOne({
+//       "name": username,
+//     });
+//     if (!rootUser) {
+//       throw new Error("User not found");
+//     }
+//     req.rootUser = rootUser;
+//   } catch (err) {
+//     res.status(401).send("Unauthorized:No token provided");
+//     console.log(err);
+//   }
+// });
 //Update Data
 router.put("/update", async (req, res) => {
   const newWork = req.body.newWork;
@@ -117,12 +150,49 @@ router.put("/update", async (req, res) => {
     console.log(error);
   }
   console.log("updated");
-  res.send("Updated")
+  res.send("Updated");
 });
-//User Data
-router.get("/userdata", authenticate, (req, res) => {
-  console.log("Hello from Userdata");
-  res.send(req.rootUser);
+//Update Pic
+router.put("/updatePic", async (req, res) => {
+  const newPic = req.body.newPic;
+  const id = req.body.id;
+  console.log(id);
+  console.log(newPic);
+  try {
+    await User.findById(id, (error, workToUpdate) => {
+      workToUpdate.pic = String(newPic);
+      workToUpdate.save();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("updated");
+  res.send("Updated");
+});
+
+//Update Data
+router.put("/edit", async (req, res) => {
+  const { name, email, phone, work, id, pic, messages} = req.body;
+  console.log(id);
+  console.log(name);
+  console.log(email);
+  console.log(phone);
+  try {
+    await User.findById(id, (error, workToUpdate) => {
+      
+      workToUpdate.pic = String(pic);
+      workToUpdate.name = String(name);
+      workToUpdate.email = String(email);
+      workToUpdate.phone = Number(phone);
+      workToUpdate.work = String(work);
+      workToUpdate.messages = String(messages);
+      workToUpdate.save();
+    });
+  } catch (error) {
+    console.log(error);
+  }
+  console.log("updated");
+  res.send("Updated");
 });
 //Contact Section
 router.post("/contact", authenticate, async (req, res) => {
