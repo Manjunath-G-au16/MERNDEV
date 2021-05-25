f8129b807169a7a96466830c8520b2c9a94e0a6c
modimanju <modimanju2019@gmail.com>
2021-05-25 08:38:12 +0530

    Fixed display all user

--- Server/middleware/displayuser.js
+++ Server/middleware/displayuser.js
@@ -1,14 +1,23 @@
 const User = require("../model/userSchema");
 
 const displayuser = async (req, res, next) => {
-const category = req.body.category
+  const category = req.body.category;
   try {
-    const rootUser = await User.find({name: category});
-    if (!rootUser) {
-      throw new Error("User not found");
+    if (category == "all") {
+      const rootUser = await User.find();
+      if (!rootUser) {
+        throw new Error("User not found");
+      }
+      req.rootUser = rootUser;
+      next();
+    } else {
+      const rootUser = await User.find({ name: category });
+      if (!rootUser) {
+        throw new Error("User not found");
+      }
+      req.rootUser = rootUser;
+      next();
     }
-    req.rootUser = rootUser;
-    next();
   } catch (err) {
     res.status(401).send("Unauthorized:No token provided");
     console.log(err);
